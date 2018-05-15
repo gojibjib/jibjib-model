e4170874845c3ac49ffe4946634fe17069aaa82e
sebie01 <36108865+sebie01@users.noreply.github.com>
2018-05-15 20:59:27 +0200

    Add files via upload

--- deploy_model.py
+++ deploy_model.py
@@ -3,52 +3,12 @@ import vggish_input
 import vggish_params
 import numpy as np
 import vggish_postprocess
-import timeit
+import timeit,os
 
 
+_NUM_CLASSES =5 
 
-
-
-
-def load_model():
-
-	#get input for model: spectogram of audio file
-	path = "/Users/Sebastian/Documents/own_projects/animal_voices/models/research/audioset/wav_files/Acrocephalus_schoenobaenus/Acrocephalus_schoenobaenus_2.wav"
-	spectrogram = vggish_input.wavfile_to_examples(path)
-
-	with tf.Session() as sess:
-		
-		#restores model
-		new_saver = tf.train.import_meta_graph('my_test_model.ckpt.meta')
-		#apparently needs to be in same repo 
-		new_saver.restore(sess, tf.train.latest_checkpoint('./'))
-		
-
-		#builds graph
-		graph = tf.get_default_graph()
-
-
-		labels = tf.placeholder(tf.float32, shape=(None, _NUM_CLASSES), name='labels')
-		
-		
-		# Locate all the tensors and ops we need for the query.
-		features_tensor = sess.graph.get_tensor_by_name(vggish_params.INPUT_TENSOR_NAME)
-		#labels_tensor = sess.graph.get_tensor_by_name('mymodel/train/labels:0')
-		
-		#init special tensor to extract embedding out of model
-		embedding_tensor = sess.graph.get_tensor_by_name(vggish_params.OUTPUT_TENSOR_NAME)
-
-		#prepare a postprocessor
-		pproc = vggish_postprocess.Postprocessor('vggish_pca_params.npz')
-
-		start = timeit.timeit()
-
-		#query model for features of provided audio input
-		[output] = sess.run ([embedding_tensor],feed_dict = {features_tensor: spectrogram})
-		postprocessed_batch = pproc.postprocess(output)
-		#print(postprocessed_batch)
-
-
+def get_seq_example(postprocessed_batch):
 		# Print the postprocessed embeddings as a SequenceExample, in a similar
 		# format as the features released in AudioSet. Each row of the batch of
 		# embeddings corresponds to roughly a second of audio (96 10ms frames), and
@@ -69,12 +29,53 @@ def load_model():
 					}
 			)
 		)
+		return seq_example
 
-		print(seq_example)
-		end = timeit.timeit()
-		print ("Time needed:")
-		print (end - start)
+def get_spectrogram(path):
+	#get input for model: spectogram of audio file
+	return vggish_input.wavfile_to_examples(path)
 
+def start_model():
+
+
+	with tf.Session() as sess:
+		
+		#restores model
+		new_saver = tf.train.import_meta_graph('my_test_model.ckpt.meta')
+		#apparently needs to be in same repo 
+		new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+		
+
+		#builds graph
+		graph = tf.get_default_graph()
+		labels = tf.placeholder(tf.float32, shape=(None, _NUM_CLASSES), name='labels')
+		# Locate all the tensors and ops we need for the query.
+		features_tensor = sess.graph.get_tensor_by_name(vggish_params.INPUT_TENSOR_NAME)
+		#init tensor to extract embedding out of model
+		embedding_tensor = sess.graph.get_tensor_by_name(vggish_params.OUTPUT_TENSOR_NAME)
+
+		#prepare a postprocessor
+		pproc = vggish_postprocess.Postprocessor('vggish_pca_params.npz')
+
+		#setting directory for scanning files
+		rootDir = "../wav_files/"
+		for dirName,subdirList, fileList in os.walk(rootDir):
+			for fname in fileList:
+				print (fname)
+		#get_input
+		spectrogram = get_spectrogram("/Users/Sebastian/Documents/own_projects/animal_voices/models/research/audioset/wav_files/Acrocephalus_schoenobaenus/Acrocephalus_schoenobaenus_2.wav")
+
+		#query model for features of provided audio input
+		[output] = sess.run ([embedding_tensor],feed_dict = {features_tensor: spectrogram})
+		postprocessed_batch = pproc.postprocess(output)
+		#print(postprocessed_batch)
+
+		seq_example = get_seq_example(postprocessed_batch)
+
+		#init writer
+		writer = tf.python_io.TFRecordWriter("my_new_file.tfrecords")
+		writer.write(seq_example.SerializeToString())
+		writer.close()
 
 if __name__ == '__main__':
-	load_model()
+	start_model()
