e19b4f17178545a3d7594c537b0e06b38614dfbf
sebie01 <36108865+sebie01@users.noreply.github.com>
2018-05-23 22:31:51 +0200

    Add files via upload

--- vggish_train_demo.py
+++ vggish_train_demo.py
@@ -59,9 +59,12 @@ import wave
 from scipy.io import wavfile
 from numpy import array
 import sklearn.model_selection as sk
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 import vggish_input
 import vggish_params
 import vggish_slim
+from numpy import array
 
 flags = tf.app.flags
 slim = tf.contrib.slim
@@ -87,6 +90,9 @@ FLAGS = flags.FLAGS
 _NUM_CLASSES = 3
 
 def random_frame(spectogram, min_size, max_size):
+  if min_size <0 or max_size <0:
+    return spectogram
+
   x,y,z = spectogram.shape
   if x >=min_size and max_size <=x:
     #picks a random few second frame from spectogram array
@@ -97,7 +103,11 @@ def random_frame(spectogram, min_size, max_size):
     return spectogram
 
 def get_one_hot(target, nb_classes):
-  return np.eye(nb_classes)[np.array(target).reshape(-1)]
+  return np.eye(nb_classes)*np.array(target).reshape(-1)
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
 
 #load spectrograms into list
 def load_spectrogram(rootDir):
@@ -113,10 +123,15 @@ def load_spectrogram(rootDir):
           #print(path)
           #calling vggish function, reads in wav file and returns mel spectrogram
           signal_example = vggish_input.wavfile_to_examples(path)
-          encoded = (get_one_hot(counter-1,_NUM_CLASSES))
-          signal_label =np.array(encoded)
+          #encoded = get_one_hot(counter-1,_NUM_CLASSES)
+          encoded = np.zeros((_NUM_CLASSES))
+          encoded[counter-1]=1
+          #encoded = tf.one_hot(counter,_NUM_CLASSES)
+          #print(encoded)
+          encoded=encoded.tolist()
+          signal_label =np.array([encoded]*signal_example.shape[0])
+          print(signal_label)
           #Shows what classes got what encoding on terminal
-          print(encoded)
           input_examples.append(signal_example)
           input_labels.append(signal_label)
     counter +=1
@@ -137,6 +152,7 @@ def get_random_batches(full_examples,input_labels,start,end):
   # Separate and return the features and labels.
   features = [example for (example, _) in labeled_examples]
   labels = [label for (_, label) in labeled_examples]
+
   return (features, labels)
 
   
@@ -155,8 +171,8 @@ def main(_):
 
       # Add a classifier layer at the end, consisting of parallel logistic
       # classifiers, one per class. This allows for multi-class tasks.
-      logits = slim.fully_connected(
-          fc, _NUM_CLASSES, activation_fn=None, scope='logits')
+      logits = slim.fully_connected(fc, _NUM_CLASSES, activation_fn=None, scope='logits')
+      
       tf.sigmoid(logits, name='prediction')
 
 
@@ -170,7 +186,7 @@ def main(_):
         # Labels are assumed to be fed as a batch multi-hot vectors, with
         # a 1 in the position of each positive class label, and 0 elsewhere.
         labels = tf.placeholder(
-            tf.float32, shape=(None, _NUM_CLASSES), name='labels')
+            tf.float32, shape=(None,_NUM_CLASSES), name='labels')
       
         # Cross-entropy label loss.
         xent = tf.nn.sigmoid_cross_entropy_with_logits(
@@ -179,16 +195,21 @@ def main(_):
         tf.summary.scalar('loss', loss)
 
         # We use the same optimizer and hyperparameters as used to train VGGish.
+        
         optimizer = tf.train.AdamOptimizer(
             learning_rate=vggish_params.LEARNING_RATE,
             epsilon=vggish_params.ADAM_EPSILON)
         optimizer.minimize(loss, global_step=global_step, name='train_op')
-      
+
+
         #Add evaluation ops
-        with tf.variable_scope("evaluation"):
-          prediction = tf.argmax(logits,1)
-          correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(labels,1))
-          accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+       # with tf.variable_scope("evaluation"):
+        prediction = tf.argmax(logits,1)
+        correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(labels,1))
+        #correct_prediction = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels = labels)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        #eval_correct = tf.nn.in_top_k(logits,labels,1) 
+
 
     # create a summarizer that summarizes loss and accuracy
     tf.summary.scalar("Accuracy", accuracy)
@@ -229,55 +250,40 @@ def main(_):
     for step in range(FLAGS.num_batches):      
       #extract random sequences for each example
       #maybe just allow very little variation
-      (X_train, y_train) = get_random_batches(X_train_entire,y_train_entire,6,9)
+      (X_train, y_train) = get_random_batches(X_train_entire,y_train_entire,-1,-1)
       
       #validation set stays the same 
-      (X_test,y_test) = get_random_batches(X_test_entire,y_test_entire,6,9)
+      (X_test,y_test) = get_random_batches(X_test_entire,y_test_entire,-1,-1)
       #implementing mini batch
       
-      minibatch_size = int(len(X_train)/1)
-      
+      minibatch_size = int(len(X_train)/5)
+      print(len(X_train))
       counter =1
       for i in range(0, len(X_train), minibatch_size):
         # Get pair of (X, y) of the current minibatch/chunk
         X_train_mini = X_train[i:i + minibatch_size]
         y_train_mini = y_train[i:i + minibatch_size]
+
+
         print("Step: "+str(step+1)+":")
         print(str(counter)+"/"+str(int(math.ceil(len(X_train)/minibatch_size))))
         
-        [num_steps, loss,_, train_acc] = sess.run([global_step_tensor, loss_tensor, train_op,accuracy],feed_dict={features_tensor: X_train_mini, labels_tensor: y_train_mini})
+        [num_steps, loss,_, train_acc,temp] = sess.run([global_step_tensor, loss_tensor, train_op,accuracy,prediction],feed_dict={features_tensor: X_train_mini, labels_tensor: y_train_mini})
         print("Loss in minibatch: "+str(loss))
         print("Training Acc in minibatch: "+str(train_acc))
         counter +=1
 
-        #every 1 steps validation accuracy
+                #every 1 steps validation accuracy
         if (num_steps)%1 == 0:
-          val_acc,pred_val = sess.run([accuracy,correct_prediction], feed_dict={features_tensor: X_test, labels_tensor: y_test})
-          print("Validation accuracy:", val_acc)
           
-          pred = sess.run([prediction],feed_dict={features_tensor:X_test})
-          print("###Input###")
-          print(y_test)
-          print("###Output###")
-          print(pred)
-          print(pred_val)
+          val_acc, = sess.run([accuracy], feed_dict={features_tensor: X_test, labels_tensor: y_test})
+          print("Validation accuracy: ")
+          print(val_acc)
+  
           
-          """
-          fmt = '{:<8}{:<20}{:<20}{}'
-          print(fmt.format('', , 'Actual Vector','True/ False'))
-          for j, (pred,y_test, pred_val) in enumerate(zip(pred,y_test, pred_val)):
-            print(fmt.format(j, pred,y_test, pred_val))
-          """
-          
-          #print( prediction.eval(feed_dict={features_tensor: X_test}))
-          #print (sess.run(prediction,{features_tensor:X_test}))
-          #print("actual input:")
-          #print(y_test)
-          #print("Validation prediction:")
-          #print(pred_val)
 
         print("###############################")
-
+        
 
 
 
