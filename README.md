430f03e87d1523e12126967a97f08b3809f74f90
obitech <obitech@users.noreply.github.com>
2018-05-30 19:18:04 +0200

    Cleaned code, wrote readme

--- README.md
+++ README.md
@@ -1,5 +1,75 @@
 Vggish_train_demo.py itereates through directories containing .wav-files, gets the labels from the directory names and finetunes a VGGish feature extractor.
 
+Deploy_model.py restores the pretrained TensorFlow model, consumes a .wav file and generates a semantically meaningful, high-level 128-D embedding. The embeddings are `tfrecord` files that can be fed into a downstream classification model later on.
 
+## Training
 
-Deploy_model.py restores the pretrained TensorFlow model, consumes a .waf file and generates a semantically meaningful, high-level 128-D embedding. The embeddings are tfrecord files that can be fed into a downstream classification model later on.
+### Docker
+
+Get the container:
+
+```
+# GPU
+docker pull obitech/jibjib-model:gpu-latest
+
+# CPU
+docker pull obitech/jibjib-model:cpu-latest
+```
+
+Get the [audioset](https://github.com/tensorflow/models/tree/master/research/audioset) checkpoint:
+
+```
+curl -O https://storage.googleapis.com/audioset/vggish_model.ckpt
+```
+
+Copy all training folders / files into `input/data/`
+
+Get the [`bird_id_map.pickle`](github.com/gojibjib/voice-grabber):
+
+```
+curl -O ./input/bird_id_map.pickle https://github.com/gojibjib/voice-grabber/raw/master/meta/bird_id_map.pickle
+```
+
+Run the container:
+
+```
+docker container run --rm -d \
+    -v $(pwd)/input:/model/input \
+    -v $(pwd)/output:/model/output \
+    obitech/jibjib-model:gpu-latest
+```
+
+### Locally
+
+Clone the repo:
+
+```
+git clone https://github.com/gojibjib/jibjib-model
+```
+
+Install dependencies:
+
+```
+pip install -r requirements.txt
+```
+
+Get the [audioset](https://github.com/tensorflow/models/tree/master/research/audioset) checkpoint:
+
+```
+curl -O https://storage.googleapis.com/audioset/vggish_model.ckpt
+```
+
+Copy all training folders / files into `input/data/`
+
+Get the [`bird_id_map.pickle`](github.com/gojibjib/voice-grabber):
+
+```
+curl -O ./input/bird_id_map.pickle https://github.com/gojibjib/voice-grabber/raw/master/meta/bird_id_map.pickle
+```
+
+Start training:
+
+```
+# Use python2
+python ./vggish_train.py
+```
--- vggish_train.py
+++ vggish_train.py
@@ -12,8 +12,6 @@ import os
 import numpy as np
 from numpy import array
 import sklearn.model_selection as sk
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
 import vggish_input
 import vggish_params
 import vggish_slim
@@ -25,8 +23,9 @@ from traceback import print_exc
 flags = tf.app.flags
 slim = tf.contrib.slim
 
+# Number of epochs
 flags.DEFINE_integer(
-    'num_batches', 15,
+    'num_batches', 10,
     'Number of batches of examples to feed into the model. Each batch is of '
     'variable size and contains shuffled examples of each class of audio.')
 
@@ -51,37 +50,23 @@ model_dir = os.path.join(output_dir, "model/")
 
 # Load pickle into bird_id_map, this dict maps Bird_name -> Database ID
 bird_id_map = {}
-map_path = os.path.join(input_dir, "bird_id_map.pickle")
+bird_id_map_path = os.path.join(input_dir, "bird_id_map.pickle")
+
+# Save train_id_list as pickle, so we can later translate back train IDs/labels (counter) to birds
+train_id_list = []
+train_id_list_path = os.path.join(output_dir, "train_id_list.pickle")
+
+pickle.HIGHEST_PROTOCOL
 try:
-  with open(map_path, "rb") as rf:
-    pickle.HIGHEST_PROTOCOL
+  with open(bird_id_map_path, "rb") as rf:
     bird_id_map = pickle.load(rf)
 except:
-  print("Unable to load {}".format(map_path))
+  print("Unable to load {}".format(bird_id_map_path))
   print_exc()
-
-# This dict maps counter ID (for internal labels) -> Bird_name
-train_id_map = {}
 
 # We need to check how many classes are present
 _NUM_CLASSES = len([name for name in os.listdir(data_dir) if not os.path.isfile(name) and name != ".empty"])
 print("Number of classes: {}".format(_NUM_CLASSES))
-
-def random_frame(spectogram, min_size, max_size):
-  if min_size <0 or max_size <0:
-    return spectogram
-
-  x,y,z = spectogram.shape
-  if x >=min_size and max_size <=x:
-    #picks a random few second frame from spectogram array
-    length = np.random.random_integers(low=min_size,high=max_size)
-    random_start = np.random.random_integers(low=0,high=x-length)
-    return spectogram[random_start:(random_start+length),0:y,0:z]
-  else:
-    return spectogram
-
-def get_one_hot(target, nb_classes):
-  return np.eye(nb_classes)*np.array(target).reshape(-1)
 
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
@@ -94,55 +79,52 @@ def load_spectrogram(rootDir):
   input_labels = []
 
   for dirName, subdirList, fileList  in os.walk(rootDir):
-    print(dirName)
     bird = os.path.basename(os.path.normpath(dirName))
     if bird == "data":
       continue
 
-    train_id_map[counter] = bird
-    # encode_pos = bird_id_map[bird]
+    print("{} -> {}".format(bird, counter))
+    train_id_list.append(bird)
     for fname in fileList:
       if fname.endswith(".wav"):
           path = os.path.join(dirName, fname)
 
           #calling vggish function, reads in wav file and returns mel spectrogram
           signal_example = vggish_input.wavfile_to_examples(path)
-          #encoded = get_one_hot(counter-1,_NUM_CLASSES)
           
           encoded = np.zeros((_NUM_CLASSES))
           encoded[counter]=1
-          #encoded[encode_pos-1]=1
 
           encoded=encoded.tolist()
           signal_label =np.array([encoded]*signal_example.shape[0])
-          print(signal_label)
+
           #Shows what classes got what encoding on terminal
           input_examples.append(signal_example)
           input_labels.append(signal_label)
     counter +=1
+
+  try:
+    with open(train_id_list_path, "wb") as wf:
+      pickle.dump(train_id_list, wf)
+  except:
+    print("Unable to dump into {}".format(train_id_list_path))
+
   return input_examples, input_labels
 
-def get_random_batches(full_examples,input_labels,start,end):
-  
-  input_examples=[]
-  for element in full_examples:
-    #gets just the 5 second sequence for each example 
-    signal_example= random_frame(element,start,end)
-    input_examples.append(signal_example)
-
-  all_examples = np.concatenate([x for x in input_examples ])
+# Shuffling data
+def get_random_batches(full_examples,input_labels):
+  all_examples = np.concatenate([x for x in full_examples ])
   all_labels = np.concatenate([x for x in input_labels])
   labeled_examples = list(zip(all_examples,all_labels))
   shuffle(labeled_examples)
+  
   # Separate and return the features and labels.
   features = [example for (example, _) in labeled_examples]
   labels = [label for (_, label) in labeled_examples]
 
   return (features, labels)
-
   
 def main(_):
-
   with tf.Graph().as_default(), tf.Session() as sess:
     # Define VGGish.
     embeddings = vggish_slim.define_vggish_slim(FLAGS.train_vggish)
@@ -159,7 +141,6 @@ def main(_):
       logits = slim.fully_connected(fc, _NUM_CLASSES, activation_fn=None, scope='logits')
       
       tf.sigmoid(logits, name='prediction')
-
 
       # Add training ops.
       with tf.variable_scope('train'):
@@ -179,22 +160,17 @@ def main(_):
         loss = tf.reduce_mean(xent, name='loss_op')
         tf.summary.scalar('loss', loss)
 
-        # We use the same optimizer and hyperparameters as used to train VGGish.
-        
+        # We use the same optimizer and hyperparameters as used to train VGGish.        
         optimizer = tf.train.AdamOptimizer(
             learning_rate=vggish_params.LEARNING_RATE,
             epsilon=vggish_params.ADAM_EPSILON)
         optimizer.minimize(loss, global_step=global_step, name='train_op')
 
-
         #Add evaluation ops
       with tf.variable_scope("evaluation"):
         prediction = tf.argmax(logits,1)
         correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(labels,1))
-        #correct_prediction = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels = labels)
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        #eval_correct = tf.nn.in_top_k(logits,labels,1) 
-
 
     # create a summarizer that summarizes loss and accuracy
     tf.summary.scalar("Accuracy", accuracy)
@@ -212,7 +188,6 @@ def main(_):
     test_writer = tf.summary.FileWriter(os.path.join(log_dir, "test/"),
                                       sess.graph)
     
-
     tf.global_variables_initializer().run()
 
     # Initialize all variables in the model, and then load the pre-trained
@@ -231,51 +206,50 @@ def main(_):
 
     #loads all input with corresponding label
     #training
-    print("Load data set...")
+    print("Loading data set and mapping birds to training IDs...")
     all_examples, all_labels =load_spectrogram(os.path.join(input_dir, "data/"))
     #creates training and test set
-    X_train_entire, X_test_entire, y_train_entire, y_test_entire = sk.train_test_split(all_examples, all_labels, test_size=0.1)
+    X_train_entire, X_test_entire, y_train_entire, y_test_entire = sk.train_test_split(all_examples, all_labels, test_size=0.2)
     
     # The training loop.
-    for step in range(FLAGS.num_batches):      
-      #extract random sequences for each example
-      #maybe just allow very little variation
-      (X_train, y_train) = get_random_batches(X_train_entire,y_train_entire,-1,-1)
+    for step in range(FLAGS.num_batches):
+      print("######## Epoch {}/{} started ########")      
+      # extract random sequences for each example
+      # maybe just allow very little variation
+      (X_train, y_train) = get_random_batches(X_train_entire,y_train_entire)
       
       #validation set stays the same 
-      (X_test,y_test) = get_random_batches(X_test_entire,y_test_entire,-1,-1)
+      (X_test,y_test) = get_random_batches(X_test_entire,y_test_entire)
       #implementing mini batch
-      
-      minibatch_size = int(len(X_train)/5)
-      print(len(X_train))
-      counter =1
+  
+      minibatch_n = 5
+      minibatch_size = int(len(X_train)/minibatch_n)
+      print("\nStarting training with {} audio frames\n".format(len(X_train)))
+      counter = 1
       for i in range(0, len(X_train), minibatch_size):
+        print("(Epoch {}/{}) ==> Minibatch {}/{} started ...".format(step+1, FLAGS.num_batches, counter, minibatch_n))
         # Get pair of (X, y) of the current minibatch/chunk
         X_train_mini = X_train[i:i + minibatch_size]
         y_train_mini = y_train[i:i + minibatch_size]
-
-
-        print("Step: "+str(step+1)+":")
-        print(str(counter)+"/"+str(int(math.ceil(len(X_train)/minibatch_size))))
         
         [summary,num_steps, loss,_, train_acc,temp] = sess.run([summary_op,global_step_tensor, loss_tensor, train_op,accuracy,prediction],feed_dict={features_tensor: X_train_mini, labels_tensor: y_train_mini})
         train_writer.add_summary(summary, step*minibatch_size+i)
         print("Loss in minibatch: "+str(loss))
-        print("Training Acc in minibatch: "+str(train_acc))
-        counter +=1
-
+        print("Training accuracy in minibatch: "+str(train_acc))
         
-        #every 1 steps validation accuracy
-        if (i)%2 == 0:
+        # Check validation accuracy every 2 steps
+        if i%2 == 0:
           summary,loss,val_acc,pred, corr_pred = sess.run([summary_op,loss_tensor,accuracy,prediction,correct_prediction], feed_dict={features_tensor: X_test, labels_tensor: y_test})
           print("Validation Accuracy: {}".format(val_acc))
           test_writer.add_summary(summary, step*minibatch_size+i)
+          
+        print("(Epoch {}/{}) ==> Minibatch {}/{} started ...".format(step+1, FLAGS.num_batches, counter, minibatch_n))
+        print()
+        counter +=1
 
-        print("###############################")
-    
     saver = tf.train.Saver()
     #Save the variables to disk.
-    save_path = saver.save(sess, os.path.join(model_dir, "jibjib_model.ckpt"),global_step=5)
+    save_path = saver.save(sess, os.path.join(model_dir, "jibjib_model.ckpt"),global_step=2)
     print("Model saved in path: %s" % save_path)
 
 if __name__ == '__main__':
