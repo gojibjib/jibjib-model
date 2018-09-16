98c687c7641dcf84356e1983cf65e8447ccd29d5
obitech <obitech@users.noreply.github.com>
2018-09-16 21:35:30 +0200

    identations, removed save directive

--- modelbuilder.py
+++ modelbuilder.py
@@ -18,50 +18,38 @@ def create_parser():
 
 	parser = argparse.ArgumentParser(description=arg_desc)
 	parser.add_argument('checkpoint',
-						help='The full path to the checkpoint ckpt file and meta file. Example: output/mymodel.ckpt-10 will use the files output/mymodel.ckpt-10 and output/mymodel.ckpt-10.meta',
-						type=str)
+		help='The full path to the checkpoint ckpt file and meta file. Example: output/mymodel.ckpt-10 will use the files output/mymodel.ckpt-10 and output/mymodel.ckpt-10.meta',
+		type=str)
 	parser.add_argument('--features_tensor',
-						help='The name of the features Tensor. Default: {}'.format(FEATURE_TENSOR),
-						type=str,
-						default=FEATURE_TENSOR)
+		help='The name of the features Tensor. Default: {}'.format(FEATURE_TENSOR),
+		type=str,
+		default=FEATURE_TENSOR)
 	parser.add_argument('--logits',
-						help='The name of the logits Tensor. Default: {}'.format(LOGITS),
-						type=str,
-						default=LOGITS)
+		help='The name of the logits Tensor. Default: {}'.format(LOGITS),
+		type=str,
+		default=LOGITS)
 	parser.add_argument('--save_path',
-						help='The path to save the serialized model to. Will create on absence. Default: {}'.format(SAVE_PATH),
-						type=str,
-						default=SAVE_PATH)
+		help='The path to save the serialized model to. Will create on absence. Default: {}'.format(SAVE_PATH),
+		type=str,
+		default=SAVE_PATH)
 	parser.add_argument('--model_name',
-						help='The name of the model. Default: {}'.format(MODEL_NAME),
-						type=str,
-						default=MODEL_NAME)
+		help='The name of the model. Default: {}'.format(MODEL_NAME),
+		type=str,
+		default=MODEL_NAME)
 	parser.add_argument('--model_version',
-						help='The model version. Default: {}'.format(VERSION),
-						type=str,
-						default=VERSION)
+		help='The model version. Default: {}'.format(VERSION),
+		type=str,
+		default=VERSION)
 	parser.add_argument('--serve_path',
-						help='The path where the model will be served from. Default: {}'.format(SERVE_PATH),
-						type=str,
-						default=SERVE_PATH)
-	
+		help='The path where the model will be served from. Default: {}'.format(SERVE_PATH),
+		type=str,
+		default=SERVE_PATH)
+
 	return parser
 
 
 args = create_parser().parse_args()
-
-# If necessary, create save dir
-if not (os.path.exists(args.save_path)):
-	try:
-		print("Creating {}".format(args.save_path))
-		os.makedirs(args.save_path)
-	except:
-		print("Unable to create {}".format(args.save_path))
-		print_exc()
-		sys.exit(1)
-
 with tf.Session(graph = loaded_graph) as sess:
-
 	try:
 		saver = tf.train.import_meta_graph("{}.meta".format(args.checkpoint))
 	except:
